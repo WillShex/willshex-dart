22c942b4290ec0d90f8aa4236cd2ee2b0ff473f8
William Shakour (billy1380) <william.shakour@gmail.com>
2020-06-10 10:37:22 +0100

    export storage commands

--- lib/willshex.dart
+++ lib/willshex.dart
@@ -22,3 +22,16 @@ export 'src/storage/storageprovider.dart';
 export 'src/utility/jsonutils.dart';
 export 'src/utility/stringutils.dart';
 export 'src/utility/typedef.dart';
+
+
+export 'src/storage/cmd/compactor.dart';
+export 'src/storage/cmd/simplequery.dart';
+export 'src/storage/cmd/deleter.dart';
+export 'src/storage/cmd/saver.dart';
+export 'src/storage/cmd/loadids.dart';
+export 'src/storage/cmd/deletetype.dart';
+export 'src/storage/cmd/deleteids.dart';
+export 'src/storage/cmd/queryexecute.dart';
+export 'src/storage/cmd/loader.dart';
+export 'src/storage/cmd/query.dart';
+export 'src/storage/cmd/loadtype.dart';
