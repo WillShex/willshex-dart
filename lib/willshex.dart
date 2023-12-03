eaa431981508b5cc2977f0ba5f5bc3801834be49
William Shakour (billy1380) <william.shakour@gmail.com>
2023-12-03 15:58:00 +0000

    moved storage into seperate package

--- lib/willshex.dart
+++ lib/willshex.dart
@@ -3,36 +3,18 @@
 /// More dartdocs go here.
 library willshex;
 
-export 'src/datatype.dart';
-export 'src/jsonable.dart';
-export 'src/pager.dart';
-export 'src/sortdirectiontype.dart';
-export 'src/sparsearray.dart';
-
 export 'src/api/abstractjsonserviceclient.dart';
 export 'src/api/error.dart';
 export 'src/api/request.dart';
 export 'src/api/response.dart';
 export 'src/api/statustype.dart';
-
-export 'src/storage/class.dart';
-export 'src/storage/cloneable.dart';
-export 'src/storage/storage.dart';
-export 'src/storage/storageprovider.dart';
-
-export 'src/utility/jsonutils.dart';
-export 'src/utility/stringutils.dart';
+export 'src/class.dart';
+export 'src/cloneable.dart';
+export 'src/jsonable.dart';
+export 'src/pager.dart';
+export 'src/sortdirectiontype.dart';
+export 'src/sparsearray.dart';
 export 'src/utility/containerhelpers.dart';
+export 'src/utility/jsonutils.dart';
 export 'src/utility/logging.dart';
-
-export 'src/storage/cmd/compactor.dart';
-export 'src/storage/cmd/simplequery.dart';
-export 'src/storage/cmd/deleter.dart';
-export 'src/storage/cmd/saver.dart';
-export 'src/storage/cmd/loadids.dart';
-export 'src/storage/cmd/deletetype.dart';
-export 'src/storage/cmd/deleteids.dart';
-export 'src/storage/cmd/queryexecute.dart';
-export 'src/storage/cmd/loader.dart';
-export 'src/storage/cmd/query.dart';
-export 'src/storage/cmd/loadtype.dart';
+export 'src/utility/stringutils.dart';
