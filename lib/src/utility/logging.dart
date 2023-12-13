2a413dc3a2691a43259ebb41bb235d7066be3f4a
William Shakour (billy1380) <william.shakour@gmail.com>
2023-12-13 09:18:21 +0000

    using stdout and stderr instead of print

--- lib/src/utility/logging.dart
+++ lib/src/utility/logging.dart
@@ -1,8 +1,26 @@
 import 'package:logging/logging.dart';
+import 'package:universal_file/universal_file.dart';
 
 void setupLogging() {
   Logger.root.level = Level.ALL; // defaults to Level.INFO
   Logger.root.onRecord.listen((LogRecord record) {
-    print("${record.level.name}: ${record.time}: ${record.message}");
+    writeLn(
+      record.level == Level.SEVERE || record.level == Level.WARNING
+          ? stderr
+          : stdout,
+      level: record.level,
+      time: record.time,
+      message: record.message,
+    );
   });
+}
+
+void writeLn(
+  Stdout out, {
+  required Level level,
+  required DateTime time,
+  required String message,
+}) {
+  out.writeln(
+      "${level.name}: ${time}: ${level == Level.SHOUT ? message.toUpperCase() : message}");
 }
