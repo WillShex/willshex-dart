d9ec7673ae83cfb67f210d66e4e3eb1f5fd1cb44
William Shakour (billy1380) <william.shakour@gmail.com>
2023-12-27 14:51:19 +0000

    reverted std out changes because they are incompatible with web

--- lib/src/utility/logging.dart
+++ lib/src/utility/logging.dart
@@ -1,26 +1,9 @@
 import 'package:logging/logging.dart';
-import 'package:universal_file/universal_file.dart';
 
 void setupLogging() {
   Logger.root.level = Level.ALL; // defaults to Level.INFO
   Logger.root.onRecord.listen((LogRecord record) {
-    writeLn(
-      record.level == Level.SEVERE || record.level == Level.WARNING
-          ? stderr
-          : stdout,
-      level: record.level,
-      time: record.time,
-      message: record.message,
-    );
+    print(
+        "${record.level.name}: ${record.time}: ${record.level == Level.SHOUT ? record.message.toUpperCase() : record.message}");
   });
-}
-
-void writeLn(
-  Stdout out, {
-  required Level level,
-  required DateTime time,
-  required String message,
-}) {
-  out.writeln(
-      "${level.name}: ${time}: ${level == Level.SHOUT ? message.toUpperCase() : message}");
 }
