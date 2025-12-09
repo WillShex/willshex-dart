4ea7a6666e9aa948e4f0336811b69cba238a30d6
William Shakour (billy1380) <william.shakour@gmail.com>
2025-12-09 11:28:45 +0000

    ignore print

--- lib/src/utility/logging.dart
+++ lib/src/utility/logging.dart
@@ -3,6 +3,7 @@ import 'package:logging/logging.dart';
 void setupLogging() {
   Logger.root.level = Level.ALL; // defaults to Level.INFO
   Logger.root.onRecord.listen((LogRecord record) {
+    // ignore: avoid_print
     print(
         "${record.level.name}: ${record.time}: ${record.level == Level.SHOUT ? record.message.toUpperCase() : record.message}");
   });
